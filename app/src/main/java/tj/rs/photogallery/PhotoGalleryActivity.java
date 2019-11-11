98685f87fcd30155ed3a3c842861aaacd98e2dee
rustamsafarovrs <rs.programmer@hotmail.com>
2019-11-11 13:53:25 +0500

    Initial commit

---
+++ app/src/main/java/tj/rs/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,12 @@
+package tj.rs.photogallery;
+
+
+import androidx.fragment.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
